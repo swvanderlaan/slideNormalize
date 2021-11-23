6bb58d9ad77db1fc2c5e3f3dcfffceb8174bbde6
swvanderlaan <s.w.vanderlaan@gmail.com>
2021-11-23 10:28:27 +0100

    compile flags correction

--- source/compile.sh
+++ source/compile.sh
@@ -1,2 +1,4 @@
-# g++ `pkg-config opencv ` -o ../binaries/sideNormalize slideNormalize.cpp
-g++ `pkg-config opencv --cflags --libs` -o ../binaries/slideNormalize slideNormalize.cpp
+# CentOS7: 
+# g++ `pkg-config opencv --cflags ` -I/hpc/local/CentOS7/dhl_ec/software/Anaconda3_2021_05/envs/cp4/include/ -o ../binaries/sideNormalize_CentOS7 slideNormalize.cpp
+# macOS Monterey
+g++ `pkg-config opencv --cflags --libs` -o ../binaries/slideNormalize_macOS slideNormalize.cpp
