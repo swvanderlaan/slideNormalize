a6fe2fe8de9161a805e65f02c66a5ade6ea20ec0
swvanderlaan <s.w.vanderlaan@gmail.com>
2021-11-23 10:25:05 +0100

    edits

--- source/slideNormalize.cpp
+++ source/slideNormalize.cpp
@@ -23,7 +23,7 @@ std::string replaceString(std::string subject, const std::string& search,
     return subject;
 }
 
-std::string remove_extension(const std::string& filename) {
+std::string remove_extension(const std::string & filename) {
     size_t lastdot = filename.find_last_of(".");
     if (lastdot == std::string::npos) return filename;
     return filename.substr(0, lastdot); 
